993646f7865fb10882ec452727d5b5176ebf4405
xbeheydt <xavier.beheydt@gmail.com>
2022-02-28 11:16:05 +0100

    add test examples

--- example/example.c
+++ example/example.c
@@ -1,6 +1,32 @@
 #include <cunits42.h>
 
-int main(int argc, char *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static cunits42_t	test_strcmp(void)
 {
-	return 0;
+	// Test OK
+	UNIT_TEST(strcmp("Hello", "Hello"), ==, strcmp("Hello", "Hello"));
+
+	// Test KO
+	UNIT_TEST(strcmp("Hello", "Hello"), ==, strcmp("hello", "Hello"));
+	return (CUNITS42_OK);
+}
+
+static cunits42_t	test_skipped(void)
+{
+	UNIT_TEST(1, !=, 2);
+	return (CUNITS42_OK);
+}
+
+static cunits42_test_t	tests[] = {
+	{ TEST_FCN(test_strcmp), NULL, NULL, CUNITS42_RUN },
+	{ TEST_FCN(test_skipped), NULL, NULL, CUNITS42_SKIP },
+	TEST_END
+};
+
+int	main(int argc, const char *argv[])
+{
+	return (main_test(tests, argc, argv));
 }
